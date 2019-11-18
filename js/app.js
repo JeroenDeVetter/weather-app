4278ecd68b87f7d80662e8a345e36749fa3da026
sanderdms <sanderdooms@live.be>
2019-11-18 12:37:15 +0100

    remove custom filter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const getWeatherData = async city => {
   const apiKey = "d02ba4169b2ac4f0d179b1e84c341147";
-  const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=2797657&APPID=${apiKey}`;
+  //const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=2797657&APPID=${apiKey}`;
+  const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`;
 
   fetch(apiUrl)
     .then(res => res.json())
@@ -12,7 +13,6 @@ const getWeatherData = async city => {
     });
 };
 
-getWeatherData()
 
 const getCityList = async () => {
   const response = await fetch("../data/city.list.json")
@@ -20,20 +20,18 @@ const getCityList = async () => {
   return data; 
 };
 
-getCityList();
 const cityInput = document.getElementById("city-input");
 const weatherForm = document.getElementById("weather-form");
 let timer = null;
 cityInput.addEventListener("keydown", function() {
   clearTimeout(timer);
   timer = setTimeout(() => {
-    getCityList()
-    .then(result => {
-       const filtered = result.filter((name) => name.name.toLowerCase() == cityInput.value.toLowerCase().trim() );
-       console.table(filtered);
-    });
- 
-  }, 200);
+
+    const result = getWeatherData(cityInput.value);
+
+    console.log(result);
+
+  }, 400);
 });
 
 weatherForm.addEventListener("submit", function(e) {
