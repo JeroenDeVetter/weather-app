dae85d812ef3a43af35daed8b4f4887ef9dc4acb
sanderdms <sanderdooms@live.be>
2019-11-18 13:50:34 +0100

    add correct metrics to api request params

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const getWeatherData = async city => {
   const apiKey = "d02ba4169b2ac4f0d179b1e84c341147";
   //const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=2797657&APPID=${apiKey}`;
-  const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`;
+  const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${apiKey}`;
 
   fetch(apiUrl)
     .then(res => res.json())
